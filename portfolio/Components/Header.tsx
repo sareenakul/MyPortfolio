b71f65d2f79108e9796c05e97101c2719a35f5d3
Akul Sareen <sareen.akul@gmail.com>
2024-05-18 14:52:50 -0400

    Added the react-spring instead of framer motion

--- portfolio/Components/Header.tsx
+++ portfolio/Components/Header.tsx
@@ -1,35 +1,37 @@
+"use client";
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
-import { motion } from "framer-motion";
+import { useSpring, animated } from '@react-spring/web';
 
 type Props = {}
 
 function Header({}: Props) {
+    const leftSpring = useSpring({
+        from: { scale: 0.5, opacity: 0, x: -500 },
+        to: { scale: 1, opacity: 1, x: 0 },
+        config: { duration: 1000 },
+    });
+    const rightSpring = useSpring({
+        from: { scale: 0.5, opacity: 0, x: 500 },
+        to: { scale: 1, opacity: 1, x: 0 },
+        config: { duration: 100 },
+    });
+
   return (
     <header className='sticky top-0 flex items-start justify-between p-5 max-w-7xl mx-auto z-20 xl:items-center'>
-        <motion.div 
-            initial={{ 
-                opacity: 0,
-                x: -500,
-                scale: 0.5
-            }}
-            animate={{ 
-                opacity: 1,
-                x: 0,
-                scale: 1
-            }}
-            className='flex flex-row items-center'>
+        <animated.div style={leftSpring}
+            className='flex flex-row items-center' >
                 {/* Social Icons */}
                 <SocialIcon url="https://www.linkedin.com/in/akuls51/" fgColor='gray' bgColor='transparent'/>
                 <SocialIcon url="https://github.com/sareenakul" fgColor='gray' bgColor='transparent'/>
                 <SocialIcon url="https://www.instagram.com/medleydiaries/" fgColor='gray' bgColor='transparent'/>
                 <SocialIcon url='https://leetcode.com/u/sareenakul/' fgColor='gray' bgColor='transparent'/>
                 <SocialIcon url='http://medleydiaries.com' fgColor='gray' bgColor='transparent'/>
-        </motion.div>
-        <div className='flex flex-row items-center cursor-pointer'>
+        </animated.div>
+        <animated.div style={rightSpring} className='flex flex-row items-center cursor-pointer'>
             <SocialIcon className='cursor-pointer' network='email' fgColor='gray' bgColor='transparent'/>
             <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
-        </div>
+        </animated.div>
     </header>
   )
 }
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Header from "@/Components/Header";
 import Head from "next/head";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
