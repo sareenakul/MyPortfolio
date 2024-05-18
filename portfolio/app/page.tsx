eacc12c8a43ae34592ef1dfda6b5c83cbf0b9d68
Akul Sareen <sareen.akul@gmail.com>
2024-05-18 03:02:02 -0400

    Setup

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Akul's Portfolio</title>
+      </Head>
+      {/* Header */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+    </div>
+  );
+}
