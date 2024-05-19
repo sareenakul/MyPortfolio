4431903ec080abdba84463a7df518a85315ce198
Akul Sareen <sareen.akul@gmail.com>
2024-05-18 20:42:46 -0400

    Done with Hero

--- portfolio/Components/BackgroundCircles.tsx
+++ portfolio/Components/BackgroundCircles.tsx
@@ -16,7 +16,7 @@ const BackgroundCircles = (props: Props) => {
   return (
     <div className='relative flex justify-center items-center'>
       <animated.div
-        style={{ ...circleProps(0, '200px', '#333333'), height: '200px', width: '200px', border: '1px solid #333333', marginTop: '13rem' }}
+        style={{ ...circleProps(100, '200px', '#333333'), height: '200px', width: '200px', border: '1px solid #333333', marginTop: '13rem' }}
         className='absolute rounded-full animate-ping'
       />
       <animated.div
@@ -24,15 +24,15 @@ const BackgroundCircles = (props: Props) => {
         className='absolute rounded-full'
       />
       <animated.div
-        style={{ ...circleProps(1000, '500px', '#333333'), height: '500px', width: '500px', border: '1px solid #333333', marginTop: '13rem' }}
+        style={{ ...circleProps(300, '500px', '#333333'), height: '500px', width: '500px', border: '1px solid #333333', marginTop: '13rem' }}
         className='absolute rounded-full'
       />
       <animated.div
-        style={{ ...circleProps(1500, '650px', '#F7AB0A'), height: '650px', width: '650px', border: '1px solid #F7AB0A', marginTop: '13rem' }}
+        style={{ ...circleProps(0, '750px', '#F7AB0A'), height: '650px', width: '650px', border: '1px solid #F7AB0A', marginTop: '13rem' }}
         className='absolute rounded-full animate-pulse'
       />
       <animated.div
-        style={{ ...circleProps(2000, '800px', '#333333'), height: '800px', width: '800px', border: '1px solid #333333', marginTop: '13rem' }}
+        style={{ ...circleProps(200, '800px', '#333333'), height: '800px', width: '800px', border: '1px solid #333333', marginTop: '13rem' }}
         className='absolute rounded-full'
       />
     </div>
--- portfolio/Components/Hero.tsx
+++ portfolio/Components/Hero.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
+import akul from "../public/Akul.jpeg";
+import akuls from "../public/Akuls.jpg";
+import akuls1 from "../public/akuls1.jpg";
+import Image from 'next/image';
+import Link from 'next/link';
 
 type Props = {}
 
 const Hero = (props: Props) => {
     const [text, count] = useTypewriter({
-        words: ["Hi, The name's Akul Sareen", "AlwaysNamesVariablesCamelCase" , "<LovesToCode>"],
+        words: ["Hi, The name's Akul Sareen", "I'm a DevLover" , "I'm a DevLoper" , "<Learn EveryDay>"],
         loop: true,
         delaySpeed: 2000,
     })
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden object-cover'>
         <BackgroundCircles/>
-        <h1>
-            <span>{text}</span>
-            <Cursor cursorColor='orange'/>
-        </h1>
+        <Image src={akul} alt='me' className='relative rounded-full h-32 w-32 mx-auto'/>
+        <div className='z-20'>
+            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Developer</h2>
+            <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                <span className='mr-3'>{text}</span>
+                <Cursor cursorColor='orange'/>
+            </h1>
+            <div className='pt-5'>
+                <Link href="#about">
+                    <button className='heroButton'>About</button>
+                </Link>
+                <Link href="#experience">
+                    <button className='heroButton'>Experience</button>
+                </Link>
+                <Link href="#skills">
+                    <button className='heroButton'>Skills</button>
+                </Link>
+                <Link href="#projects">
+                <button className='heroButton'>Projects</button>
+                </Link>
+            </div>
+        </div>
     </div>
   )
 }
