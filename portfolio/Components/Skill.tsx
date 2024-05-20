9fdcafeb692f0738836f98e91d40d052f56ed254
Akul Sareen <sareen.akul@gmail.com>
2024-05-20 12:34:53 -0400

    Working over stylling of skills

--- portfolio/Components/Skill.tsx
+++ portfolio/Components/Skill.tsx
@@ -21,8 +21,13 @@ const Skill = ({directionLeft}: Props) => {
         transition={{
             duration: 1,
         }}
-        className='rounder-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+            <div className='flex items-center justify-center h-full'>
+                <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+            </div>
+        </div>
     </div>
   )
 }
--- portfolio/Components/Skills.tsx
+++ portfolio/Components/Skills.tsx
@@ -21,7 +21,7 @@ const Skills = (props: Props) => {
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
 
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>Hover over skill for current proficiency</h3>
-        <div className='grid grid-cols-4 gap-5'>
+        <div className='pt-36 grid grid-cols-4 gap-5'>
             <Skill/>
             <Skill/>
             <Skill/>
