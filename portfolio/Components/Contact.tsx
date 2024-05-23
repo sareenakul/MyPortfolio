0c0ba10ca6470fec74c5a46c1f52d9ec4dabb66a
Akul Sareen <sareen.akul@gmail.com>
2024-05-23 03:31:40 -0400

    Fixing Style

--- portfolio/Components/Contact.tsx
+++ portfolio/Components/Contact.tsx
@@ -12,7 +12,9 @@ type Inputs = {
 
 const Contact = (props: Props) => {
     const { register, handleSubmit} = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = formData => console.log(formData);
+  const onSubmit: SubmitHandler<Inputs> = formData =>{
+    window.location.href = `mailto:akuls18@my.yorku.ca?subject=${formData.subject}&body=Hi, I'm ${formData.name}. ${formData.message}`;
+  }
   return (
     <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
