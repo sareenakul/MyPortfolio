5f872957ff6e836d4ee34cdef1531d20daf98914
Akul Sareen <sareen.akul@gmail.com>
2024-05-31 22:35:27 -0400

    Hero image transition

--- portfolio/Components/Hero.tsx
+++ portfolio/Components/Hero.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 import akul from "../public/Akul.jpeg";
-import akuls from "../public/Akuls.jpg";
-import akuls1 from "../public/akuls1.jpg";
 import Image from 'next/image';
 import Link from 'next/link';
+import two from "../public/photosMe/hero2.jpg";
+import six from "../public/photosMe/hero6.jpg";
+import seven from "../public/photosMe/hero7.jpg";
 
 type Props = {}
 
@@ -17,34 +18,52 @@ const Hero = (props: Props) => {
         " <Coder>"],
         loop: true,
         delaySpeed: 2000,
-    })
-  return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden object-cover'>
-        <BackgroundCircles/>
-        <Image src={akul} alt='me' className='relative rounded-full h-32 w-32 mx-auto'/>
-        <div className='z-20'>
-            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Developer</h2>
-            <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
-                <span className='mr-3'>Hi, I'm a{text}</span>
-                <Cursor cursorColor='orange'/>
-            </h1>
-            <div className='pt-5'>
-                <Link href="#about">
-                    <button className='heroButton'>About</button>
-                </Link>
-                <Link href="#experience">
-                    <button className='heroButton'>Experience</button>
-                </Link>
-                <Link href="#skills">
-                    <button className='heroButton'>Skills</button>
-                </Link>
-                <Link href="#projects">
-                <button className='heroButton'>Projects</button>
-                </Link>
+    });
+
+    const images = [akul, two, six, seven];
+    const [currentImage, setCurrentImage] = useState(0);
+    const [animating, setAnimating] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setAnimating(true);
+            setTimeout(() => {
+                setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+                setAnimating(false);
+            }, 0); // 1 second animation
+        }, 100); // Change image every 3 seconds
+        return () => clearInterval(interval);
+    }, [images.length]);
+
+    return (
+        <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden object-cover'>
+            <BackgroundCircles />
+            <div className={`relative rounded-full h-32 w-32 mx-auto ${animating ? 'image-transition' : ''}`}>
+                <Image src={images[currentImage]} alt='me' layout='fill' objectFit='cover' className='rounded-full' />
+            </div>
+            <div className='z-20'>
+                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Developer</h2>
+                <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                    <span className='mr-3'>Hi, I'm a{text}</span>
+                    <Cursor cursorColor='orange'/>
+                </h1>
+                <div className='pt-5'>
+                    <Link href="#about">
+                        <button className='heroButton'>About</button>
+                    </Link>
+                    <Link href="#experience">
+                        <button className='heroButton'>Experience</button>
+                    </Link>
+                    <Link href="#skills">
+                        <button className='heroButton'>Skills</button>
+                    </Link>
+                    <Link href="#projects">
+                        <button className='heroButton'>Projects</button>
+                    </Link>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Hero
+export default Hero;
