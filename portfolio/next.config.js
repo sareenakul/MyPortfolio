6b56c5475e9bffef057c1810fb08bb671b2d7eef
Akul Sareen <sareen.akul@gmail.com>
2024-11-12 00:51:10 -0500

    Trial

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -2,38 +2,39 @@
 
 import test from 'node:test';
 
-// const nextConfig = {
-//     output: 'export',
-//     images: {
-//         unoptimized: true, // Disable Image Optimization API for static export
-//     },
-//     trailingSlash: true,
-// };
-
-// export default nextConfig;
-
 const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true, // Disable Image Optimization API for static export
     },
     trailingSlash: true,
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /|.scss$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                {
-                    loader: 'saas-loader',
-                    options:{
-                        api: 'modern',
-                    }
-                }
-            ]
-        })
-    }
 };
+
+// export default nextConfig;
+
+// const nextConfig = {
+//     output: 'export',
+//     images: {
+//         unoptimized: true, // Disable Image Optimization API for static export
+//     },
+//     trailingSlash: true,
+//     webpack: (config) => {
+//         config.module.rules.push({
+//             test: /|.scss$/,
+//             use: [
+//                 'style-loader',
+//                 'css-loader',
+//                 {
+//                     loader: 'saas-loader',
+//                     options:{
+//                         api: 'modern',
+//                     }
+//                 }
+//             ]
+//         })
+//         return config;
+//     }
+// };
 
 export default nextConfig;
 
