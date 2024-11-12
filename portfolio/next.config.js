897cbae8f8c1910bbc4220175fb99cad10ed92d7
Akul Sareen <sareen.akul@gmail.com>
2024-11-11 22:46:18 -0500

    changed nextConfig

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,14 +1,40 @@
 /** @type {import('next').NextConfig} */
 
+// const nextConfig = {
+//     output: 'export',
+//     images: {
+//         unoptimized: true, // Disable Image Optimization API for static export
+//     },
+//     trailingSlash: true,
+// };
+
+// export default nextConfig;
+
 const nextConfig = {
     output: 'export',
     images: {
-        unoptimized: true, // Disable Image Optimization API for static export
+      unoptimized: true,
     },
-    trailingSlash: true,
-};
-
-export default nextConfig;
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              api: 'modern', // Specify the modern API for Dart Sass
+            },
+          },
+        ],
+      });
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
 
 
 
