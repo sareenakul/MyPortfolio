c8c0c347870c0c6e2c37808b378724645042420b
Akul Sareen <sareen.akul@gmail.com>
2024-11-25 03:16:05 -0500

    Changes

--- portfolio/tailwind.config.cjs
+++ portfolio/tailwind.config.cjs
@@ -3,18 +3,19 @@ const config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        // "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        // "gradient-conic":
-        //   "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [
-    // require('tailwind-scrollbar'),
+    require('tailwind-scrollbar'),
   ],
 };
 
