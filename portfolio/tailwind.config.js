2c3ed875db4c2fe15241313da65b583c19012d39
Akul Sareen <sareen.akul@gmail.com>
2024-12-04 14:57:28 -0500

    Finally resolved the issue by fixing name of Component. Now experiment with the extend

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -22,11 +22,11 @@ export default {
   
   theme: {
     extend: {
-      // backgroundImage: {
-      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //   "gradient-conic":
-      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [
