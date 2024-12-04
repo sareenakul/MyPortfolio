e92aa0c69c24164b11b47038ea8ebc84d06431b1
Akul Sareen <sareen.akul@gmail.com>
2024-12-03 22:36:39 -0500

    use instead of import

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Lato } from "next/font/google";
 import "./globals.css";
-// import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 const lato = Lato({ subsets: ["latin"], weight: ["400", "700"] });
