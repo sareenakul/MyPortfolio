0158a9961ccc767f84dd9e6ea684c3a1d48c81f9
Akul Sareen <sareen.akul@gmail.com>
2024-12-04 12:46:16 -0500

    hit-trial-9

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,49 +1,34 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   //works
-//   // content: [
-//   //   "./components/**/*.{js,ts,jsx,tsx,mdx}",
-//   //   "./app/**/*.{js,ts,jsx,tsx,mdx}",
-//   //   "./public/**/*.html",
-//   // ],
-//   // dont work
-//   // content: [
-//   //   './app/**/*.{js,ts,jsx,tsx}',
-//   //   './app/Components/**/*.{js,ts,jsx,tsx}',
-//   //   "./public/**/*.html",
-//   // ],
-//   content: [
-//     "./app/**/*.{js,ts,jsx,tsx,mdx}", // All files in the 'app' folder
-//     "./components/**/*.{js,ts,jsx,tsx,mdx}", // All files in the 'components' folder
-//     "./pages/**/*.{js,ts,jsx,tsx,mdx}", // If you have a 'pages' folder
-//     "./public/**/*.html", // Any HTML files in 'public' (if applicable)
-//   ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  //works
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx,css,scss}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx,css}",
+    "./public/**/*.html",
+  ],
+  // dont work
+  // content: [
+  //   './app/**/*.{js,ts,jsx,tsx,css}',
+  //   './app/Components/**/*.{js,ts,jsx,tsx,scss}',
+  //   "./public/**/*.{html,png,pdf,svg}",
+  // ],
+  // content: [
+  //   "./app/**/*.{js,ts,jsx,tsx,mdx}", // All files in the 'app' folder
+  //   "./components/**/*.{js,ts,jsx,tsx,mdx}", // All files in the 'components' folder
+  //   "./pages/**/*.{js,ts,jsx,tsx,mdx}", // If you have a 'pages' folder
+  //   "./public/**/*.html", // Any HTML files in 'public' (if applicable)
+  // ],
   
-//   theme: {
-//     extend: {
-//       backgroundImage: {
-//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-//         "gradient-conic":
-//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-//       },
-//     },
-//   },
-//   plugins: [
-//     require('tailwindcss'),
-//   ],
-// }
-
-//Not working for deployment
-// module.exports = {
-//   mode: 'jit',
-//   content: [
-//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./public/**/*.html",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss'),
+  ],
+}
