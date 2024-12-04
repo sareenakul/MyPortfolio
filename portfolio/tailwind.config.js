17e0ea0b5e6213815e429ad741a5a6aefeb89749
Akul Sareen <sareen.akul@gmail.com>
2024-12-04 12:52:26 -0500

    hit-trial-10

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -21,11 +21,11 @@ export default {
   
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
     },
   },
   plugins: [
